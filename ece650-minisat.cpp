26727ecce304b836b75fb0a41c99bdcaade2e17e
Bethelhem Nibret <bnibret@WSDL14.eng.uwaterloo.ca>
2023-03-26 11:19:39 -0400

    second v

--- ece650-minisat.cpp
+++ ece650-minisat.cpp
@@ -8,93 +8,97 @@
 
 using namespace std;
 
-int CNF_SAT(int n, int k, vector<vector<int>>edges){
+int CNF_SAT(int n, vector<vector<int>>edges){
     unique_ptr<Minisat::Solver> solver(new Minisat::Solver());
     vector<int> vertexCover; 
+    int k=1; 
 
-    //create an array of literals of size n*k
-    Minisat::Lit literals[k][n]; 
+    while (k<n+1)
+    {
+        //create an array of literals of size n*k
+        Minisat::Lit literals[k][n]; 
 
-    //create n*k positive literals over n*k new variables
-    for (int i=0; i<n; i++){
-        for (int j=0; j<k; j++){
-            literals[j][i] = Minisat::mkLit(solver->newVar()); 
-        }
-    }
-
-    //1. Create the first set of clauses
-    //at least one vertex is the ith vertex in the vertex cover:
-    for (int i=0; i<k; i++){
-        Minisat::vec<Minisat::Lit> tempVar;
-        for (int j=0; j<n; j++){
-            tempVar.push(literals[i][j]);
-        }
-        solver->addClause(tempVar); //also try without using tempCLAUSE 
-    }
-
-    //2. Create the second set of clauses 
-    //No one vertex can appear twice in a vertex cover
-    for (int m=0; m<n; m++){
-        for (int p=0; p<k-1; p++){
-            for (int q=p+1; q<k; q++ ){
-                solver->addClause(~literals[p][m], ~literals[q][m]); 
+        //create n*k positive literals over n*k new variables
+        for (int i=0; i<n; i++){
+            for (int j=0; j<k; j++){
+                literals[j][i] = Minisat::mkLit(solver->newVar()); 
             }
         }
-    }
 
-     //3. Create the third set of clauses
-     //No more than one vertex appears in the mth position of the vertex cover
-    for(int m=0; m<k; m++){
-        for(int p=0; p<n-1; p++){
-            for(int q=p+1; q<n; q++){
-                solver->addClause(~literals[m][p], ~literals[m][q]); 
-            }
-        }
-    }
-
-    //4. create the fouth set of clauses
-    //Every edge is incident to at least one vertex in the vertex cover
-    //the atomic prop is true iff the vertext end/start is the mth vertext in the vertext cover
-    for (int l=0; l<edges.size(); l++){
-        Minisat::vec<Minisat::Lit> tempVar;
-        int start = edges[l][0];
-        int end = edges[l][1];
-        for (int m=0; m<k; m++){
-            tempVar.push(literals[m][end]);
-            tempVar.push(literals[m][start]);
-        }
-        solver -> addClause(tempVar); 
-    }
-
-    //check if F is satisfiable. If so, print vertext cover and exit
-    bool result = solver->solve();
-    if(result==1){
-        for (int i=0; i<k ; i++){
+        //1. Create the first set of clauses
+        //at least one vertex is the ith vertex in the vertex cover:
+        for (int i=0; i<k; i++){
+            Minisat::vec<Minisat::Lit> tempVar;
             for (int j=0; j<n; j++){
-                if (Minisat::toInt(solver->modelValue(literals[i][j]))==0){
-                    vertexCover.push_back(j);
+                tempVar.push(literals[i][j]);
+            }
+            solver->addClause(tempVar); //also try without using tempCLAUSE 
+        }
+
+        //2. Create the second set of clauses 
+        //No one vertex can appear twice in a vertex cover
+        for (int m=0; m<n; m++){
+            for (int p=0; p<k-1; p++){
+                for (int q=p+1; q<k; q++ ){
+                    solver->addClause(~literals[p][m], ~literals[q][m]); 
                 }
             }
         }
 
-        sort(vertexCover.begin(),vertexCover.end());
-        for (int i=0; i<vertexCover.size(); i++){
-            if (i==vertexCover.size()-1){
-                cout<<vertexCover[i]<<endl;
-            }
-            else{
-                cout<<vertexCover[i]<<" ";
+        //3. Create the third set of clauses
+        //No more than one vertex appears in the mth position of the vertex cover
+        for(int m=0; m<k; m++){
+            for(int p=0; p<n-1; p++){
+                for(int q=p+1; q<n; q++){
+                    solver->addClause(~literals[m][p], ~literals[m][q]); 
+                }
             }
         }
+
+        //4. create the fouth set of clauses
+        //Every edge is incident to at least one vertex in the vertex cover
+        //the atomic prop is true iff the vertext end/start is the mth vertext in the vertext cover
+        for (unsigned int l=0; l<edges.size(); l++){
+            Minisat::vec<Minisat::Lit> tempVar;
+            int start = edges[l][0];
+            int end = edges[l][1];
+            for (int m=0; m<k; m++){
+                tempVar.push(literals[m][end]);
+                tempVar.push(literals[m][start]);
+            }
+            solver -> addClause(tempVar); 
+        }
+
+        //check if F is satisfiable. If so, print vertext cover and exit
+        bool result = solver->solve();
+        if(result==1){
+            for (int i=0; i<k ; i++){
+                for (int j=0; j<n; j++){
+                    if (Minisat::toInt(solver->modelValue(literals[i][j]))==0){
+                        vertexCover.push_back(j);
+                    }
+                }
+            }
+
+            sort(vertexCover.begin(),vertexCover.end());
+            for (unsigned int i=0; i<vertexCover.size(); i++){
+                if (i==vertexCover.size()-1){
+                    cout<<vertexCover[i]<<endl;
+                }
+                else{
+                    cout<<vertexCover[i]<<" ";
+                }
+            }
+            solver.reset (new Minisat::Solver());
+            return 1;    
+        }
+
+        //else continue with the next k element
         solver.reset (new Minisat::Solver());
-        return 1;    
+        k++; 
     }
 
-    //continue with the next k element
-    else {
-        solver.reset (new Minisat::Solver());
-        return 0;
-    }
+    return 0; 
 }
 
 
@@ -155,13 +159,14 @@ int main(){
                 edges.push_back(nums); 
             }
 
-            int result; 
-            for (int i=1; i<vertex_no+1; i++){
-                int result = CNF_SAT(vertex_no,i, edges); 
-                if (result==1){ //means CNF SAT successful 
-                    break; 
-                }
-            }
+            CNF_SAT(vertex_no,edges);
+            // int result; 
+            // for (int i=1; i<vertex_no+1; i++){
+            //     int result = CNF_SAT(vertex_no,i, edges); 
+            //     if (result==1){ //means CNF SAT successful 
+            //         break; 
+            //     }
+            // }
         }
     }
 }
